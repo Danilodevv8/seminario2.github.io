63d6b2572f6de9a67f2a66e5dedaf3b5c4f15b33
Danilo Almeida <166953020+Danilodevv8@users.noreply.github.com>
2024-06-13 11:40:46 -0300

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,46 +2,38 @@
 const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicatorInput = document.querySelector("#multiplicator");
+const multiplicationTitle = document.querySelector("#multiplication-title span");
+const multiplicationTable = document.querySelector("#multiplication-operations");
 
-
-const multiplicationTitle = document.querySelector(
-    "#multiplication-title span"
-);
-
-
-const multiplicationTable = document.querySelector("#multiplication-operations"
-
-);
 // Funções
 const createTable = (number, multiplicatorNumber) => {
     multiplicationTable.innerHTML = "";
     for (let i = 1; i <= multiplicatorNumber; i++) {
         const result = number * i;
 
-        const template = `<div class="row">  
-            <div class="operation">${number} x ${i} = </div> 
+        const template = `<div class="row">
+            <div class="operation">${number} x ${i} = </div>
             <div class="result">${result}</div>
         </div>`;
+        
+        // Criar o elemento a partir do template
         const parser = new DOMParser();
-        const htmlTemplate = parser.parseFromString(template, "text/html")
-        const row = htmlTemplate.querySelector(".row") // Extrair o primeiro filho do body (a div com classe "row")
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+        const row = htmlTemplate.querySelector(".row");
         multiplicationTable.appendChild(row);
     }
     multiplicationTitle.innerText = number;
 };
 
-
-
 // Eventos
 multiplicationForm.addEventListener("submit", (e) => {
-    e.preventDefault(); // Corrigido: preventDefault()
-
+    e.preventDefault(); // Prevenir comportamento padrão do formulário
 
     const multiplicationNumber = +numberInput.value;
-
     const multiplicatorNumber = +multiplicatorInput.value;
 
     if (!multiplicationNumber || !multiplicatorNumber) return;
 
     createTable(multiplicationNumber, multiplicatorNumber);
 });
+
